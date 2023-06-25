69235a6d4718e2a1e956fee322401fbe478932ef
Keiichi Matsuzaki <matsuzaki.keiichi@jaxa.jp>
2023-06-25 18:05:16 +0900

    fixed bug of test script (fixed too early client start up after server setup)

--- test/test-client2server.sh
+++ test/test-client2server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-../bin/pcap-nc -l 1234 --after=5 > out.pcap &
+../bin/pcap-nc -l 1234 >out.pcap &
+sleep 1
 ../bin/pcap-nc 127.0.0.1 1234 < test.pcap >/dev/null &
+
 tail -f out.pcap
